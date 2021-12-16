740e280d87852c791587b7a448202b62322c6858
pablojjimenez <pablojjimenez0@gmail.com>
2021-12-16 16:30:12 +0100

    refactor names

--- download.js
+++ download.js
@@ -11,29 +11,19 @@ const db = admin.firestore();
 const toFile = async(id) => {
 
     const ref = db.collection('topics').doc(id);
-    // const ref = db.collection('quizzes').doc(id);
 
     const data = await ref.get().then(v => v.data());
 
     const str = yaml.stringify(data, 8);
 
-    await fs.outputFile(`topics/${id}.yaml`, str);
-    // await fs.outputFile(`quizzes/${id}.yaml`, str);
+    await fs.outputFile(`data/${id}.yaml`, str);
 
     console.log('DONE')
 
 }
 
 const topics = [
-    'angular',
-    'flutter',
-    'cf',
-    'firebase',
-    'firestore',
-    'flutter',
-    'rxjs',
-    'js',
-    'ts'
+    'users'
 ]
 
 
